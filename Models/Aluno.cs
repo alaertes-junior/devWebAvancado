49e63924374c35cec5fb552c0e41ffad9a849144
Lorenzinndev <lo.laskawski@gmail.com>
2026-04-23 20:07:44 -0300

    DEVWEB feat implementado aluno controller, models e repository

--- Models/Aluno.cs
+++ Models/Aluno.cs
@@ -3,6 +3,16 @@ namespace devWebAvancado.Models
 {
     public class Aluno
     {
-        
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Cpf { get; set; }
+        public int Idade { get; set; }
+        public int CursoId { get; set; }
+        public Curso Curso { get; set; }
+        public int FrequenciaId { get; set; }
+        public Frequencia Frequencia { get; set; }
+        public int NotaId { get; set; }
+        public Nota Nota { get; set; }
     }
 }
